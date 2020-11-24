a759bd14b6a7c98ee9decb0cddeed157217b1a29
jingxual <54549856+jingxual@users.noreply.github.com>
2020-11-24 03:23:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,33 +24,45 @@ fBest=15
 def MVNSH(runMax,fBest):
     run = 0
     start = time.time()
-    
+    bestknown = []
     while run <= runMax:
+        ss = time.time()
+        print("Initializing...")
         s,f,order = initial_schedule2(d_im,P_prec,num_r,R_r,U_imr)
         makespan = max(f.flatten())
         if makespan == fBest:
+            bestknown.append(makespan)
             break
         else:
+            print("Shaking...")
             ord_list = [o[0] for o in list(order.values())]
             feas_uni = swap([ord_list])
 
-            u_s,u_f,u_makespan = modified_makespan(feas_uni[0],d_im,P_prec,num_r,R_r,U_imr)
+            print("Local searching...")
+            idx = random.randint(0,len(feas_uni)-1)
+            feas_uni = feas_uni[idx]
+            print(feas_uni)
+            u_s,u_f,u_makespan = modified_makespan(feas_uni,d_im,P_prec,num_r,R_r,U_imr)
 
             if u_makespan == fBest:
-                makespan = u_makespan
+                bestknown.append(u_makespan)
                 break
             else:
-                makespan = min(makespan, u_makespan)
+                bestknown.append(min(makespan, u_makespan))
                 run += 1
-    
+        ee = time.time()
+        print("[run]: ", run)
+        print("[time]: ", ee-ss)
+        print("makespan = ", min(bestknown))
+
     end = time.time()
     tspan = end-start
 
-    print("run: ", run)
-    print("makespan: ", makespan)
-    print("time: ", tspan)
+    print("total run: ", run)
+    print("makespan: ", min(bestknown))
+    print("total time: ", tspan)
 
-    return makespan, tspan
+    return min(bestknown), tspan
 
 if __name__ == "__main__":
     MVNSH(runMax,fBest)
