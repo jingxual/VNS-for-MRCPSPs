42ba2dbd991bc9cd7e9436fc44264a3d564d2209
jingxual <54549856+jingxual@users.noreply.github.com>
2020-11-23 21:37:32 -0500

    Update MVNSH_MRCPSPs.py

--- MVNSH_MRCPSPs.py
+++ MVNSH_MRCPSPs.py
@@ -8,4 +8,30 @@ import matplotlib.pyplot as plt
 
 import pdb
 
+import 
 path = "/content/gdrive/My Drive/635project/"
+
+
+runMax=30
+fBest=15
+
+def MVNSH(runMax,fBest):
+    run = 0
+    # makespan = 0
+    while run <= runMax:
+        s,f,order = initial_schedule2(d_im,P_prec,num_r,R_r,U_imr)
+        makespan = max(f.flatten())
+        if makespan == fBest:
+            break
+        else:
+            feas_uni = swap([order])
+
+            u_s,u_f,u_makespan = modified_makespan(feas_uni[0],d_im,P_prec,num_r,R_r,U_imr)
+
+            if u_makespan == fBest:
+                break
+            else:
+                makespan = min(makespan, u_makespan)
+                run += 1
+
+    return makespan
